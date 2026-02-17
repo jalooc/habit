92bccb904c2df2a17b77bfe923ed884620dd5deb
jalooc <jalooc@gmail.com>
2026-02-17 21:26:27 +0100

    Setup persistent storage for the stores

--- src/stores.ts
+++ src/stores.ts
@@ -1,4 +1,6 @@
 import { observable } from '@legendapp/state'
+import { synced } from "@legendapp/state/sync"
+import { ObservablePersistMMKV } from '@legendapp/state/persist-plugins/mmkv'
 
 type Id = string
 type HabitId = Id
@@ -8,11 +10,23 @@ export const habits$ = observable<
   Record<HabitId, {
     name: string,
   }>
->({})
+>(synced({
+  initial: {},
+  persist: {
+    name: "habits",
+    plugin: ObservablePersistMMKV
+  }
+}))
 
 export const groups$ = observable<
   Record<GroupId, {
     name: string,
     habits: Record<HabitId, true>
   }>
->({})
+>(synced({
+  initial: {},
+  persist: {
+    name: "groups",
+    plugin: ObservablePersistMMKV
+  }
+}))
