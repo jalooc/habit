24d97c98efd52ff2a77623472a14a00da77e484c
jalooc <jalooc@gmail.com>
2026-05-03 00:12:38 +0200

    Extract navigation definition to dedicated module

--- App.tsx
+++ App.tsx
@@ -3,26 +3,17 @@ import 'src/domains/notifications/utils/notifications'
 import 'src/domains/habits/utils/habitsNotificationsScheduler'
 import { useEffect } from 'react'
 import {
-  createStaticNavigation,
   DefaultTheme,
-  StaticParamList,
   useNavigationContainerRef,
 } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Linking } from 'react-native'
 import * as ExpoLinking from 'expo-linking'
 import * as Notifications from 'src/domains/notifications/utils/notifications'
-import Home from 'src/domains/misc/screens/Home'
-import Group from 'src/domains/habits/screens/Group'
-import DevTools from 'src/domains/devTools/screens/DevTools'
-import DevLog from 'src/domains/devTools/screens/DevLog'
-import DevLogEntry from 'src/domains/devTools/screens/DevLogEntry'
-import Backup from 'src/domains/devTools/screens/Backup'
 import { useReactNavigationDevTools } from '@dev-plugins/react-navigation'
 import { setupNotifications } from 'src/domains/notifications/utils/notifications'
 import { devLog } from 'src/domains/devTools/utils/devLog'
 import { isNonNullish } from 'remeda'
-import { groupScreenLinkPlaceholder } from 'src/domains/habits/utils/linking'
+import { Navigation } from 'src/domains/misc/utils/navigation'
 
 const navigationTheme = {
   ...DefaultTheme,
@@ -35,56 +26,6 @@ const navigationTheme = {
     border: theme.colors.border,
   },
 }
-
-const RootStack = createNativeStackNavigator({
-  screenOptions: {
-    headerShadowVisible: false,
-    headerBackTitleVisible: false,
-  },
-  screens: {
-    Home: {
-      screen: Home,
-      options: { headerShown: false },
-    },
-    Group: {
-      screen: Group,
-      options: { headerTitle: '' },
-      linking: {
-        path: groupScreenLinkPlaceholder,
-        initialRouteName: 'Home',
-      },
-    },
-    DevTools: {
-      screen: DevTools,
-      options: { headerTitle: 'Dev Tools' },
-    },
-    DevLog: {
-      screen: DevLog,
-      options: { headerTitle: 'Log' },
-    },
-    DevLogEntry: {
-      screen: DevLogEntry,
-      options: { headerTitle: 'Log entry' },
-    },
-    Backup: {
-      screen: Backup,
-      options: { headerTitle: 'Backup' },
-    },
-  },
-})
-
-type RootStackParamList = StaticParamList<typeof RootStack>
-
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace ReactNavigation {
-    // eslint-disable-next-line @typescript-eslint/consistent-type-definitions, @typescript-eslint/no-empty-object-type
-    interface RootParamList extends RootStackParamList {
-    }
-  }
-}
-
-const Navigation = createStaticNavigation(RootStack)
 
 const App = () => {
   const navigationRef = useNavigationContainerRef()
---
+++ src/domains/misc/utils/navigation.ts
@@ -0,0 +1,59 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Home from 'src/domains/misc/screens/Home'
+import Group from 'src/domains/habits/screens/Group'
+import { groupScreenLinkPlaceholder } from 'src/domains/habits/utils/linking'
+import DevTools from 'src/domains/devTools/screens/DevTools'
+import DevLog from 'src/domains/devTools/screens/DevLog'
+import DevLogEntry from 'src/domains/devTools/screens/DevLogEntry'
+import Backup from 'src/domains/devTools/screens/Backup'
+import { createStaticNavigation, StaticParamList } from '@react-navigation/native'
+
+const RootStack = createNativeStackNavigator({
+  screenOptions: {
+    headerShadowVisible: false,
+    headerBackTitleVisible: false,
+  },
+  screens: {
+    Home: {
+      screen: Home,
+      options: { headerShown: false },
+    },
+    Group: {
+      screen: Group,
+      options: { headerTitle: '' },
+      linking: {
+        path: groupScreenLinkPlaceholder,
+        initialRouteName: 'Home',
+      },
+    },
+    DevTools: {
+      screen: DevTools,
+      options: { headerTitle: 'Dev Tools' },
+    },
+    DevLog: {
+      screen: DevLog,
+      options: { headerTitle: 'Log' },
+    },
+    DevLogEntry: {
+      screen: DevLogEntry,
+      options: { headerTitle: 'Log entry' },
+    },
+    Backup: {
+      screen: Backup,
+      options: { headerTitle: 'Backup' },
+    },
+  },
+})
+
+type RootStackParamList = StaticParamList<typeof RootStack>
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace ReactNavigation {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-definitions, @typescript-eslint/no-empty-object-type
+    interface RootParamList extends RootStackParamList {
+    }
+  }
+}
+
+export const Navigation = createStaticNavigation(RootStack)
